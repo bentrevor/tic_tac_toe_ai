e4ee5c7c95a11cf0fe659a7d039e943404196624
Ben Trevor <benjamin.trevor@gmail.com>
2013-04-15 14:02:16 -0500

    Require files in tic_tac_toe_ai.rb

--- lib/tic_tac_toe_ai.rb
+++ lib/tic_tac_toe_ai.rb
@@ -1,4 +1,7 @@
 require "tic_tac_toe_ai/version"
+require "tic_tac_toe_ai/board"
+require "tic_tac_toe_ai/computer_player"
+require "tic_tac_toe_ai/minimax"
 
 module TicTacToeAi
   # Your code goes here...
